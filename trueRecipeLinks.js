d76534fcda2f76069a62b52654d5efaad540f312
Ronnie Villarini <ronnie@ronini.dev>
2024-06-15 20:55:10 -0500

    refactor to loop

--- trueRecipeLinks.js
+++ trueRecipeLinks.js
@@ -4,35 +4,33 @@ import recipeLinks from "./recipeLinks.json" assert { type: "json" };
 
 // Launch the browser and open a new blank page
 const browser = await puppeteer.launch({
-  headless: false,
-  slowMo: 250, // slow down by 250ms
+  // headless: false,
+  // slowMo: 250, // slow down by 250ms
 });
 
-const page = await browser.newPage();
-await page.goto(recipeLinks[1]);
-
-// TODO: Refactor into loop
 const trueURL = [];
-// We have to try a couple of different checks here, as samsung doesn't always load the full original recipe page
-const dataFrame = await page.waitForSelector(".s39241").catch(() => {
-  console.log("no iframe data loaded.");
-});
+for (const link of recipeLinks) {
+  const page = await browser.newPage();
+  await page.goto(link);
 
-// If the data was loaded in Samsung's container, grab the url from the data attr
-if (dataFrame) {
-  trueURL.push(await page.$eval(".s39241", (e) => e.data));
+  // We have to try a couple of different checks here, as samsung doesn't always load the full original recipe page
+  // If the data was loaded in Samsung's container, grab the url from the data attr
+  const dataFrame = await page.waitForSelector(".s39241").catch(); // Don't really need to do anything with this if it fails
+  if (dataFrame) {
+    const dataUrl = await dataFrame.evaluate(".s39241", (e) => e.data);
+    console.log(`Adding link, ${dataUrl} to list via data attribute`);
+    trueURL.push(dataUrl);
+  }
+
+  // Otherwise, just grab the URL
+  if (!dataFrame) {
+    const currentUrl = page.url();
+    console.log(`Adding link, ${currentUrl} to list via URL`);
+    trueURL.push(currentUrl);
+  }
+
+  await page.close();
 }
-
-// Otherwise, just grab the URL
-if (!dataFrame) {
-  const currentUrl = page.url();
-  trueURL.push(currentUrl);
-}
-
-// await page.click(".s40084 a");
-// await page.waitForNavigation();
-// const currentUrl = page.url();
-// console.log(currentUrl);
 
 await browser.close();
 
